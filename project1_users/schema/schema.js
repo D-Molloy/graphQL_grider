669db140e9d05200bc3f46b9ec268336542e859c
Denis Molloy <denis.molloy@gmail.com>
2018-09-28 14:46:42 -0700

    updated schemas so they reference each other

--- project1_users/schema/schema.js
+++ project1_users/schema/schema.js
@@ -1,11 +1,18 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList
+} = graphql;
 const axios = require("axios");
 
-// order of definitions is important!!
+// order of definitions is important!! (Hoisting)
+//  in order to get around UserType being undefined in the users field, wrap the fields in a far arrow
 const CompanyType = new GraphQLObjectType({
   name: "Company",
-  fields: {
+  fields: () => ({
     id: {
       type: GraphQLString
     },
@@ -14,15 +21,24 @@ const CompanyType = new GraphQLObjectType({
     },
     description: {
       type: GraphQLString
+    },
+    users: {
+      // need to specify that it will get a LIST of users
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(res => res.data);
+      }
     }
-  }
+  })
 });
 
 // 2 required properties: name (string) / fields (object) -- all the names of the props the User has
 // schemas - what the data looks like
 const UserType = new GraphQLObjectType({
   name: "User",
-  fields: {
+  fields: () => ({
     id: {
       type: GraphQLString
     },
@@ -33,6 +49,7 @@ const UserType = new GraphQLObjectType({
       type: GraphQLInt
     },
     company: {
+      //  getting the 1 company the person works for
       type: CompanyType,
       //parentValue is the user we just found
       resolve(parentValue, args) {
@@ -41,7 +58,7 @@ const UserType = new GraphQLObjectType({
           .then(res => res.data);
       }
     }
-  }
+  })
 });
 
 // the injection point for all queries
@@ -58,6 +75,16 @@ const RootQuery = new GraphQLObjectType({
         //return a particular user
         return axios
           .get(`http://localhost:3000/users/${args.id}`)
+          .then(resp => resp.data);
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        // return a specific company
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
           .then(resp => resp.data);
       }
     }
