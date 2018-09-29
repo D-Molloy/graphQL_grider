6d58fc8cb0867b1652bcb5b75a8f5a39faf3fdd4
Denis Molloy <denis.molloy@gmail.com>
2018-09-28 23:34:49 -0700

    finished project 1

--- project1_users/server.js
+++ project1_users/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const _ = require("lodash");
 const expressGraphQL = require("express-graphql");
+//
+// start json-server in another terminal with `npm run json:server`
+//
 
 //schema
 const schema = require("./schema/schema");
